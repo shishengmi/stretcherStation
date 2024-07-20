87c8f8080fef57b6f76cfa6d2a70ad073234eb27
shishengmi <shishengmi@outlook.com>
2024-07-20 17:34:52 +0800

    删除无用方法，修改变量名

--- data_processing.py
+++ data_processing.py
@@ -13,19 +13,21 @@ class DataProcessor:
         self.processed_data_ecg_web = queue.Queue()
         self.processed_data_ecg_monitor = queue.Queue()
         self.is_running = False
-        self.data_A_list = []
+        self.ecg_data_original_list = []
         self.lock = threading.Lock()
         self.lttb_n_count = 40
         self.frequency = 500
+        self.heart_rate = 0
+        self.rr_interval = 0
 
     def start(self):
         self.is_running = True
-        threading.Thread(target=self.process_data, daemon=True).start()
+        threading.Thread(target=self.process_ecg_data, daemon=True).start()
 
     def stop(self):
         self.is_running = False
 
-    def process_data(self):
+    def process_ecg_data(self):
         ecg_point_max = float('-inf')
         ecg_point_min = float('inf')
         ecg_point_max_new = 0
@@ -35,12 +37,12 @@ class DataProcessor:
         ecg_points = [3]
         ticks_heart_rate = time.time()
         ticks_heart_rate_new = 0
-        heart_rate = 0
+        peak_interval_num = 0  # 波峰的间隔点个数
 
         while self.is_running:
             try:
                 a_value = self.parser.data_A.get(timeout=1)
-                self.data_A_list.append(a_value)
+                self.ecg_data_original_list.append(a_value)
 
                 ticks_new = time.time()
                 # 刷新最大值与最小值
@@ -61,49 +63,35 @@ class DataProcessor:
                     ecg_point_min_new = float('inf')
                     ticks_old = ticks_new
 
-                if len(ecg_points) >= 3:
+                # ------------------------------心率的计算
+                elif len(ecg_points) < 3:  # 小于3代表数据初始数据未收集完成
+                    ecg_points.append(a_value)
+                if len(ecg_points) >= 3:  # 大于等于代表可以用于波峰检测
                     ecg_points.pop(0)
                     ecg_points.append(a_value)
-                    if ecg_points[0] < ecg_points[1] > ecg_points[2]:
-                        # ticks_heart_rate_new = time.time()
-                        # heart_rate = 1 / (ticks_heart_rate_new - ticks_heart_rate)
-                        # print(heart_rate)
+                    if ecg_points[0] < ecg_points[1] > ecg_points[2]:  # 检测到波峰处理逻辑
+                        peak_interval_num = 0  # 波峰间隔点个数清零
+                        self.heart_rate = 60 / (1 / self.frequency * peak_interval_num)  # 计算心率
+                        self.rr_interval = 1 / self.heart_rate  # 计算RR间隔计算
+                    else:  # 未检测到波峰
+                        peak_interval_num += 1
 
-                    # TODO 计算两次波峰的数据点，然后与已知频率计算
-                elif len(ecg_points) < 3:
-                    ecg_points.append(a_value)
+                # ------------------------------HRV心率变异度是否需要再开一个线程
 
-
-
-
-
-
-                if len(self.data_A_list) == 500:
+                # ------------------------------图像的压缩与队列处理
+                if len(self.ecg_data_original_list) == 500:
                     times = np.linspace(0, 499, num=500)
-                    simplified_data_A = lttb.downsample(np.column_stack((times, self.data_A_list)), self.lttb_n_count)
-                    processed_A = [float(point[1]) for point in simplified_data_A]
-                    self.process_data_A(processed_A)
-                    for point in processed_A:
+                    temp_list = lttb.downsample(np.column_stack((times, self.ecg_data_original_list)),
+                                                self.lttb_n_count)
+                    ecg_data_reduced_list = [float(point[1]) for point in temp_list]
+                    for point in ecg_data_reduced_list:
                         self.processed_data_ecg_web.put(point)
                         self.processed_data_ecg_monitor.put(point)
-                    print(f"Processed and stored data A: {processed_A}")  # 调试信息
-                    self.data_A_list = []
+                    print(f"Processed and stored data A: {ecg_data_reduced_list}")  # 调试信息
+                    self.ecg_data_original_list = []
 
             except queue.Empty:
                 continue
-
-    def process_data_A(self, data):
-        with self.lock:
-            while not self.processed_data_A.empty():
-                self.processed_data_A.get()
-            self.processed_data_A.put(data)
-
-    # TODO 计算心率
-    # TODO 合理化体温与血氧
-
-    def get_processed_data(self):
-        with self.lock:
-            return list(self.processed_data_A.queue) if not self.processed_data_A.empty() else []
 
     def get_ecg_data_web(self):
         with self.lock:
